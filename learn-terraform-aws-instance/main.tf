2c67e3dcbf8ea0bd411161b9abb2b5378f259514
Eva Vides <esvides@unicesar.edu.co>
2023-11-08 14:58:34 -0500

    Store remote state

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  cloud {
+    organization = "videseva"
+    workspaces {
+      name = "workspaceEva"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
